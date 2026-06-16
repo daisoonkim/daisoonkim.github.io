3fccebb691fc587da14b785cbbd13c201a5a6166
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-06-16 21:02:42 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@89697761c636964592af9ac477e9950a1eae6e80 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -453,14 +453,14 @@ ninja.data = [{
               window.location.href = "/projects/5_project/";
             },},{id: "projects-jun-2026",
           title: 'Jun 2026',
+          description: "Most robo-advisers will never profit from Wall Street’s AI-generated stock picks",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-jun-2026",
+          title: 'Jun 2026',
           description: "AI financial advisors are blind to market opportunity",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
