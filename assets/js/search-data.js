444a6068b6a3d3ddafdccf04d1db98ccaef8f61b
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-06-16 19:56:33 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@197346ca4490cd65b1a34cbb3577840f3d7e634d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-voxeu-columns-17-mar-2025",
+          title: 'VoxEU Columns 17 Mar 2025',
+          description: "The effects of parental leave policy reforms on fertility and gender gaps",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
