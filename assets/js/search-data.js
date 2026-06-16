8b0cc97ed1c53fc639f1c9410789add1ce6441e1
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-06-16 20:16:22 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@4e1308545a83421ebfbcd751e96b45dfbe9d05ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,9 +431,9 @@ ninja.data = [{
           description: "Global connectedness and market power make firms more resilient to domestic COVID-19 shocks (17 Mar 2025)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-poole-thought-leadership",
+          title: 'Poole Thought Leadership',
+          description: "The (real) weight of words (9 April 2024)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
