3cacc48d543b642fc30b1d81b504f1b41a42f14d
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-06-16 20:58:38 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@4bb8405cf9d7832210181500db66cf4d50a179aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
           description: "Economic sanctions; Lessons from the Russia-Ukraine conflict",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-arc-blog",
-          title: 'ARC Blog',
-          description: "AI financial advisors are blind to market opportunity (3 June 2026)",
+            },},{id: "projects-jun-2026",
+          title: 'Jun 2026',
+          description: "AI financial advisors are blind to market opportunity",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
