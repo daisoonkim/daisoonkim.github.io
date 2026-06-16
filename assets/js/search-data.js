cc74b78ab15e5c2b54a135f062fb845633520b9e
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-06-16 20:32:42 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@d011d97fc90e263f2ce498b6fd690c9b1d83f0d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,7 +416,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-column",
+          section: "News",},{id: "projects-interview",
+          title: 'Interview',
+          description: "Complete U.S.-China decoupling impossible... Xi is more resilient than Trump (1 May 2025)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/11_project/";
+            },},{id: "projects-column",
           title: 'Column',
           description: "The effects of parental leave policy reforms on fertility and gender gaps (17 Mar 2025)",
           section: "Projects",handler: () => {
@@ -456,11 +461,6 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
