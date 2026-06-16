a2b075c9f257cc5694fa8290ee43f4f130b0e7d5
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-06-16 21:14:53 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@8b24af3b1d5cc86ad735431eaeb148052e82af12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-articles",
           title: "ARTICLES",
-          description: "Blogs, Columns, Notes",
+          description: "News Articles, Columns, Notes, Briefs",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
