8d88d2bd92b678766d9e55ca6868314924b11441
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-06-23 14:32:21 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@43f6e05ad694a71f16a22bf93032a5f3f6eaf21f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -460,7 +460,7 @@ ninja.data = [{
           title: 'Jun 2026',
           description: "Most robo-advisers will never profit from Wall Street’s AI-generated stock picks",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-jun-2026",
           title: 'Jun 2026',
           description: "AI financial advisors are blind to market opportunity",
