7990f0235caedfe5243a9cf867ff9da49283ef0e
daisoonkim <144251330+daisoonkim@users.noreply.github.com>
2026-07-21 00:19:07 +0000

    Deploying to gh-pages from @ daisoonkim/daisoonkim.github.io@17a6b87874e1f9b4e3a8f3e5ab20d97fb6eda02e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,6 +418,11 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-jul-2026",
           title: 'Jul 2026',
+          description: "(Market Watch) South Korean stocks weren&#39;t a big deal in your emerging-markets fund. Artificial intelligence changed that.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/097_proejct/";
+            },},{id: "projects-jul-2026",
+          title: 'Jul 2026',
           description: "(The Arc Blog) The hidden choices behind emerging-market ETFs",
           section: "Projects",handler: () => {
               window.location.href = "/projects/196_project/";
@@ -466,11 +471,6 @@ ninja.data = [{
           description: "(KIEP World Economy Brief) Covid-19 crisis and shifts in the corporate competitive landscape; Comparisons with previous economic crises",
           section: "Projects",handler: () => {
               window.location.href = "/projects/499_project/";
-            },},{id: "projects-jul-2026",
-          title: 'Jul 2026',
-          description: "(Market Watch) South Korean stocks weren&#39;t a big deal in your emerging-markets fund. Artificial intelligence changed that.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/97_proejct/";
             },},{id: "projects-jun-2026",
           title: 'Jun 2026',
           description: "(Market Watch) Most robo-advisers will never profit from Wall Street’s AI-generated stock picks",
